65c613ee12b59d041de4aa0e81406bb4a10ead92
David Köppl <david.koeppl75@gmail.com>
2023-03-18 17:36:33 +0100

    adds rebass

--- supreme-portfolio/src/App.tsx
+++ supreme-portfolio/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { StyledTitle } from './styledComponents';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { StyledTitle } from './pages/styledComponents';
+import { Home } from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
-      <StyledTitle>Hello, World!</StyledTitle>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/projects"
+            element={<StyledTitle>projects</StyledTitle>}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ supreme-portfolio/src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Text } from 'rebass';
+
+export const Home = () => {
+  return (
+    <Box>
+      <Text fontSize={[3, 4, 5]} fontWeight="bold" color="primary">
+        Hello, Rebass!
+      </Text>
+    </Box>
+  );
+};
