2b3d68c60965d238e1389274bd9a256f98a8e182
David Köppl <david.koeppl75@gmail.com>
2023-03-20 00:47:43 +0100

    add images

--- supreme-portfolio/src/hooks/useProjects.ts
+++ supreme-portfolio/src/hooks/useProjects.ts
@@ -1,5 +1,8 @@
 import { ALL, Tag } from '../pages/projects/Projects.types';
 import React, { useMemo } from 'react';
+import code from '../images/code.jpg';
+import headphones from '../images/headphones.jpg';
+import universe from '../images/universe.png';
 
 export type ProjectItem = {
   projectName: string; // name of the project that's showcased
@@ -17,128 +20,92 @@ function useProjects(): ProjectItem[] {
     const projects = [
       {
         projectName: 'Gale–Shapley-Algorithm Algodat',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [code],
         tags: ['SWE'],
       },
       {
         projectName: 'A* Algorithm Algodat',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [headphones],
         tags: ['SWE'],
       },
       {
         projectName: 'Movie Podcast audividprod',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [universe],
         tags: ['MUSIC'],
       },
       {
         projectName: 'AVL search tree Algodat',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [code],
         tags: ['SWE'],
       },
       {
         projectName: 'Barnes-Hut-Algorithm EP2',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [headphones],
         tags: ['SWE'],
       },
       {
         projectName: 'Rasterizer EVC',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [universe],
         tags: ['CG'],
       },
       {
         projectName: 'Image Processing EVC',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [code],
         tags: ['CV'],
       },
       {
         projectName: 'Photggrammetry and 3D Modelling EVC',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [headphones],
         tags: ['CG', 'CV'],
       },
       {
         projectName: 'Traffic Sign Recognition EDBV',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [universe],
         tags: ['CV'],
       },
       {
         projectName: 'Fashion App Design IID',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [code],
         tags: ['UI'],
       },
       {
         projectName: 'Paypal design IID',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [headphones],
         tags: ['UI'],
       },
       {
         projectName: 'Greed ogl cg',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [universe],
         tags: ['CG', 'MUSIC', 'GAMEDEV'],
       },
       {
         projectName: 'mail server ds',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [code],
         tags: ['SWE'],
       },
       {
         projectName: 'volume rendering Vis1',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [headphones],
         tags: ['CG', 'WEBDEV'],
       },
       {
         projectName: 'Ticketline SEpm',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [universe],
         tags: ['SWE', 'WEBDEV'],
       },
       {
         projectName: 'Supreme Portfolio',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [code],
         tags: ['WEBDEV'],
       },
       {
         projectName: 'Resume CV',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [headphones],
         tags: ALL,
       },
       {
         projectName: 'WOLUM Music',
-        images: [
-          'https://cdn.shopify.com/s/files/1/0644/6861/5398/products/SW67_SS23_WorldFamousHoodedSweatshirt_Purple_720x.jpg?v=1678822302',
-        ],
+        images: [universe],
         tags: ['MUSIC'],
       },
     ] as ProjectItem[];
