b0808f083c2b3d0af55d5d2b142e7c627e21655c
David Köppl <david.koeppl75@gmail.com>
2023-03-20 23:18:59 +0100

    add portfolio

--- supreme-portfolio/src/data/portfolio.tsx
+++ supreme-portfolio/src/data/portfolio.tsx
@@ -1,17 +1,28 @@
-import code from '../images/code.jpg';
+import img1 from '../images/portfolio.png';
 import { Text } from 'rebass';
+import StyledHref from '../components/StyledHref';
 
 const portfolio = {
   id: 'portfolio',
   projectName: 'Supreme Portfolio Website',
-  images: [code],
+  images: [img1],
   tags: ['WEBDEV'],
-  technologies: ['React'],
+  technologies: ['React,', 'Typescript,', 'Rebass'],
   price: 128,
   description: (
     <Text>
-      This is a sample description for the World Famous Hooded Sweatshirt. You
-      can update this description with the actual product details.
+      I needed to update my old Wordpress portfolio, so I had the idea of
+      recreating a famous online shop in React and now you are looking at it. It
+      was styled with styled components and some components from Rebass and is
+      deployed with Netlify from the{' '}
+      <StyledHref
+        href={'https://github.com/Masdaofdisasda/react-supreme-portfolio'}
+        text={'GitHub repo'}
+      />
+      .
+      <br />
+      <br />
+      Supreme please don't sue me.
     </Text>
   ),
 };
