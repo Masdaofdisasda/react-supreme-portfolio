8afd96310bac828ff70f34b29435ad02ccd14208
David Köppl <david.koeppl75@gmail.com>
2023-03-20 17:59:22 +0100

    add greed

--- supreme-portfolio/src/data/greedGame.tsx
+++ supreme-portfolio/src/data/greedGame.tsx
@@ -1,17 +1,43 @@
-import universe from '../images/universe.png';
+import img1 from '../images/greed1.jpg';
+import img2 from '../images/greed2.jpg';
+import img3 from '../images/greed3.jpg';
 import { Text } from 'rebass';
+import StyledHref from '../components/StyledHref';
 
 const greedGame = {
   id: 'greedGame',
   projectName: 'Greed',
-  images: [universe],
+  images: [img1, img2, img3],
   tags: ['CG', 'GAMEDEV'],
-  technologies: ['OpenGL', 'C++'],
+  technologies: ['OpenGL,', 'C++'],
   price: 128,
   description: (
     <Text>
-      This is a sample description for the World Famous Hooded Sweatshirt. You
-      can update this description with the actual product details.
+      Greed is a 3D game where you have have to collect gold to win, but the
+      more you collect the slower you will be. A friend and I developed this
+      game, which got featured on the TU Wien computer graphics {''}
+      <StyledHref
+        href={'https://www.cg.tuwien.ac.at/courses/CG/HallOfFame/2022S'}
+        text={'Hall of Fame'}
+      />
+      .
+      <br />
+      <br />I developed the graphics engine using OpenGL 4.6 with the newer AZDO
+      features like indirect rendering and bindless textures. The engine
+      supports PCF shadow maps, HDR rendering and tone mapping as well as color
+      grading and even particle simulations and volumetric lights. It can als
+      use KTX textures and do frustum culling
+      <br />
+      <br /> It can be downloaded either from the TU Wien page or directly from
+      the{' '}
+      <StyledHref
+        href={'https://github.com/Masdaofdisasda/cgue22-greed'}
+        text={'GitHub repo'}
+      />
+      . Greed should run on any machine as long as it has dedicated graphics
+      card. We also created a{' '}
+      <StyledHref href={'https://youtu.be/l_ucd9Kc4S4'} text={'trailer'} /> for
+      it.
     </Text>
   ),
 };
--- supreme-portfolio/src/pages/home/components/Background.tsx
+++ supreme-portfolio/src/pages/home/components/Background.tsx
@@ -1,3 +1,4 @@
+import bg from '../../../images/background.png';
 import styled from 'styled-components';
 
 const BackgroundContainer = styled.div`
@@ -6,11 +7,10 @@ const BackgroundContainer = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background: url('https://images.unsplash.com/photo-1678257355149-6eda1755b1a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80')
-    no-repeat center center fixed;
+  background: url(${bg}) no-repeat center center fixed;
   background-size: cover;
   z-index: -1;
-  filter: brightness(0.27);
+  filter: brightness(0.1);
 `;
 
 const Background: React.FC = () => {
