25473f28a2df0c51def3ffbaa408d3be4eae2b8a
David Köppl <david.koeppl75@gmail.com>
2023-03-20 23:38:23 +0100

    add rasterizer

--- supreme-portfolio/src/data/rasterizer.tsx
+++ supreme-portfolio/src/data/rasterizer.tsx
@@ -1,17 +1,26 @@
-import universe from '../images/universe.png';
+import img1 from '../images/rasterizer1.png';
+import img2 from '../images/rasterizer2.png';
 import { Text } from 'rebass';
+import StyledHref from '../components/StyledHref';
 
 const rasterizer = {
   id: 'rasterizer',
-  projectName: 'Rasterizer',
-  images: [universe],
+  projectName: 'Software Rasterizer',
+  images: [img1, img2],
   tags: ['CG'],
   technologies: ['Matlab'],
   price: 128,
   description: (
     <Text>
-      This is a sample description for the World Famous Hooded Sweatshirt. You
-      can update this description with the actual product details.
+      As part of a lab{' '}
+      <StyledHref
+        href={'https://github.com/Masdaofdisasda/evc-rasterizer'}
+        text={'assignment'}
+      />{' '}
+      I had to implement a software rasterizer in a specified framework. This
+      helps to better understand the rendering pipeline. This assignment
+      included line and fill rasterization, color and depth interpolation and
+      viewport clipping.
     </Text>
   ),
 };
