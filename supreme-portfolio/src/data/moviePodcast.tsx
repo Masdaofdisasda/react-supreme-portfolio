887ddfa69ed8da2d0a1bb818edbc0586aa4f36b7
David Köppl <david.koeppl75@gmail.com>
2023-03-20 22:18:08 +0100

    add podcast

--- supreme-portfolio/src/data/moviePodcast.tsx
+++ supreme-portfolio/src/data/moviePodcast.tsx
@@ -1,17 +1,25 @@
-import universe from '../images/universe.png';
+import img1 from '../images/behindThePlot1.png';
 import { Text } from 'rebass';
+import StyledHref from '../components/StyledHref';
 
 const moviePodcast = {
   id: 'moviePodcast',
-  projectName: 'groupname? Podcast',
-  images: [universe],
+  projectName: 'Behind the Plot Horror Podcast',
+  images: [img1],
   tags: ['MUSIC'],
   technologies: ['Ableton'],
   price: 128,
   description: (
     <Text>
-      This is a sample description for the World Famous Hooded Sweatshirt. You
-      can update this description with the actual product details.
+      Movies are great especially horror movies but most of them have a horrible
+      story. That's what started{' '}
+      <StyledHref
+        href={'https://soundcloud.com/user-658205794'}
+        text={'this podcast'}
+      />
+      , which is about three guys reviewing lesser known spooky movies. The
+      intro and outro jingle is from me and if you find the audio to be badly
+      mixed that's also on me.
     </Text>
   ),
 };
