3c198e2b4a1a33d80ab5f54a9a88e5e92533ea7c
David Köppl <david.koeppl75@gmail.com>
2023-04-09 16:39:04 +0200

    setup camera and shadows

--- supreme-portfolio/src/pages/home/Home.tsx
+++ supreme-portfolio/src/pages/home/Home.tsx
@@ -5,8 +5,7 @@ import { Logo } from '../../components/Logo';
 import useNavItems from '../../hooks/useNavItems';
 import StyledLink from '../../components/StyledLink';
 import { SocialLinks } from './components/SocialLinks';
-import { Canvas } from '@react-three/fiber';
-import { useGLTF } from '@react-three/drei';
+import Background from './components/Background';
 
 const List = styled.ul`
   list-style-type: none;
@@ -18,17 +17,6 @@ const Item = styled.li`
   padding: 4px 0;
 `;
 
-const StyledCanvas = styled(Canvas)`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: -1;
-  background-size: cover;
-  filter: brightness(1);
-`;
-
 const Overlay = styled(Flex)`
   position: fixed;
   top: 0;
@@ -38,25 +26,12 @@ const Overlay = styled(Flex)`
   z-index: 1;
 `;
 
-function Model() {
-  const gltf = useGLTF('/models/Sponza.gltf');
-  return <primitive object={gltf.scene} position={[0, 0, 0]} />;
-}
-
 const Home: React.FC = () => {
   const menuItems = useNavItems();
 
   return (
     <>
-      <StyledCanvas
-        gl={{ antialias: false }}
-        dpr={[1, 1.5]}
-        camera={{ position: [0, 10, 0], fov: 50, near: 0.01, far: 100 }}
-      >
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Model />
-      </StyledCanvas>
+      <Background />
       <Overlay
         flexDirection={'column'}
         alignItems={'center'}
--- supreme-portfolio/src/pages/home/components/Background.tsx
+++ supreme-portfolio/src/pages/home/components/Background.tsx
@@ -1,6 +1,24 @@
 import styled from 'styled-components';
 import { useGLTF } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, extend, useFrame, useThree } from '@react-three/fiber';
+import React, { useEffect } from 'react';
+import { PerspectiveCamera, Vector3 } from 'three';
+
+extend({ PerspectiveCamera });
+
+const Camera: React.FC = () => {
+  const { camera } = useThree();
+  const typedCamera = camera as PerspectiveCamera;
+
+  useFrame(() => typedCamera.updateMatrixWorld());
+
+  useEffect(() => {
+    typedCamera.position.set(8, 1.8, 0.5);
+    typedCamera.lookAt(new Vector3(0, 1.8, 0));
+  }, [typedCamera]);
+
+  return null;
+};
 
 const StyledCanvas = styled(Canvas)`
   position: fixed;
@@ -12,18 +30,32 @@ const StyledCanvas = styled(Canvas)`
   background-size: cover;
   filter: brightness(1);
 `;
-
 function Model() {
   const gltf = useGLTF('/models/Sponza.gltf');
+
+  useEffect(() => {
+    gltf.scene.traverse((child) => {
+      if (child.isObject3D) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    });
+  }, [gltf]);
+
   return <primitive object={gltf.scene} position={[0, 0, 0]} />;
 }
 
 const Background: React.FC = () => {
   return (
-    <StyledCanvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+    <StyledCanvas gl={{ antialias: false }} dpr={[1, 1.5]} shadows>
+      <directionalLight
+        intensity={5}
+        position={[2, 20, 1]}
+        castShadow={true}
+        shadow-mapSize={1024}
+      />
       <Model />
+      <Camera />
     </StyledCanvas>
   );
 };
